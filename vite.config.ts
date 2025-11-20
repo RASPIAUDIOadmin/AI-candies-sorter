940068db76de18f84ae2c5cfb35bdea6b7291f0f
RASPIAUDIOadmin <olivier@ros.fr>
2025-11-20 17:27:30 +0100

    fix(index.html): Simplify Tailwind CDN comment
    The comment for the Tailwind CSS CDN link was slightly shortened to "Tailwind via CDN" for conciseness.
    
    fix(vite.config.ts): Update base path for GitHub Pages deployment
    
    The `base` configuration in `vite.config.ts` was updated from `'./'` to `'/AI-candies-sorter/'`. This is crucial for correctly deploying the application to GitHub Pages, as it specifies the correct subdirectory for the application's assets.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // "base: './'" permet au site de fonctionner peu importe le nom du dépôt GitHub
-  base: './', 
+  // IMPORTANT : Ceci doit correspondre EXACTEMENT au nom de votre dépôt GitHub
+  base: '/AI-candies-sorter/', 
 })
